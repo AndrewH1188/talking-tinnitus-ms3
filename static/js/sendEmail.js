e0dfcbdd422183c9ac47246eca3e3eb7e8cb2c93
AndrewH1188 <andrewh1188@gmail.com>
2021-12-19 17:19:28 +0000

    Using EmailJS to add the JavaScript from their site to enable my contact form to work when this is wired up.

---
+++ static/js/sendEmail.js
@@ -0,0 +1,22 @@
+// Taken from EmailJS 
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_u297kwq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send';
+      alert('Thank you for contacting us. We have recieved your message and will reply shortly');
+    }, (err) => {
+      btn.value = 'Send';
+      alert(JSON.stringify(err));
+    });
+});
